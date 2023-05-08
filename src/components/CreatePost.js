234eb89a129bb94dcae153994c895a55c6017df3
畔上高典 <t.azegami213@gmail.com>
2023-05-09 01:13:50 +0900

    update

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './CreatePost.css';
 
 const CreatePost = () => {
   return (
     <div className="createPostPage">
-      <div className="postContainer"></div>
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを記入" />
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea placeholder="投稿内容を記入" />
+        </div>
+        <button className="postButton">投稿する</button>
+      </div>
     </div>
   );
 };
